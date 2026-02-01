ac5237b2ce5acae5923e41947a23d3599f958679
brodybigwood <brodybigwood@gmail.com>
2026-01-31 23:50:10 -0800

    view image or video by id

--- server/wwwroot/script.js
+++ server/wwwroot/script.js
@@ -1,15 +1,40 @@
 
-async function authenticateAPI() {
-    const api_key = document.getElementById("api-key").value;
+const mainMediaContainer = document.getElementById('main-media-container');
 
-    const response = await fetch("/api", {
-        method: 'GET',
-        headers: {
-            'X-API-KEY': api_key,
-            'Content-Type': 'application/json'
-        }
+async function getAuthenticatedUrl(url) {
+
+    const api_key = document.getElementById("api-key").value;
+    const response = await fetch(url, {
+        headers: { 'X-API-KEY': api_key } 
     });
 
-    const text = await response.text();
-    alert(text);
+    if (!response.ok) throw new Error(`Auth failed: ${response.status}`);
+
+    const blob = await response.blob();
+    return URL.createObjectURL(blob);
+}
+
+async function getMediaData(id) {
+    const api_key = document.getElementById("api-key").value;
+    const response = await fetch(`/api/media/${id}/data`, {
+        headers: { 'X-API-KEY': api_key }
+    });
+    return await response.json();
+}
+
+async function loadMedia(id) {
+    const data = await getMediaData(id);
+    console.log(data);
+    const fileUrl = await getAuthenticatedUrl(`/api/media/${id}/file`);
+
+    if (data.media_type == "PHOTO") {
+        mainMediaContainer.innerHTML = `<img src="${fileUrl}"></img>`;
+    } else {
+        mainMediaContainer.innerHTML = `<video src="${fileUrl}" controls></video>`;
+    }
+}
+
+function refresh() {
+    const id = document.getElementById("media-id").value;
+    loadMedia(id);
 }
