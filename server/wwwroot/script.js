864e30c9c647b1e3f6006a70a12baab17a79a41c
brodybigwood <brodybigwood@gmail.com>
2026-02-01 01:12:00 -0800

    filter by date range on ui

--- server/wwwroot/script.js
+++ server/wwwroot/script.js
@@ -1,12 +1,17 @@
 
 const mainMediaContainer = document.getElementById('main-media-container');
 
-async function getAuthenticatedUrl(url) {
-
+async function getAuthenticatedResponse(url) {
     const api_key = document.getElementById("api-key").value;
     const response = await fetch(url, {
-        headers: { 'X-API-KEY': api_key } 
+        headers: { 'X-API-KEY': api_key }
     });
+
+    return response;
+}
+
+async function getAuthenticatedUrl(url) {
+    const response = await getAuthenticatedResponse(url);
 
     if (!response.ok) throw new Error(`Auth failed: ${response.status}`);
 
@@ -15,26 +20,59 @@ async function getAuthenticatedUrl(url) {
 }
 
 async function getMediaData(id) {
-    const api_key = document.getElementById("api-key").value;
-    const response = await fetch(`/api/media/${id}/data`, {
-        headers: { 'X-API-KEY': api_key }
-    });
+    const url = `/api/media/${id}/data`;
+    const response = await getAuthenticatedResponse(url);
     return await response.json();
 }
 
-async function loadMedia(id) {
+async function loadMedia(id, div) {
     const data = await getMediaData(id);
     console.log(data);
     const fileUrl = await getAuthenticatedUrl(`/api/media/${id}/file`);
 
     if (data.media_type == "PHOTO") {
-        mainMediaContainer.innerHTML = `<img src="${fileUrl}"></img>`;
+        div.innerHTML = `<img src="${fileUrl}"></img>`;
     } else {
-        mainMediaContainer.innerHTML = `<video src="${fileUrl}" controls></video>`;
+        div.innerHTML = `<video src="${fileUrl}" controls></video>`;
     }
 }
 
 function refresh() {
     const id = document.getElementById("media-id").value;
-    loadMedia(id);
+    loadMedia(id, mainMediaContainer);
 }
+
+async function getIdRangeInt(start, end) {
+    const url = `/api/media/index/range/${start}/${end}`;
+    const response = await getAuthenticatedResponse(url);
+    return await response.json();
+}
+
+function loadMediaArray(ids) {
+   
+    mainMediaContainer.innerHTML = '';
+ 
+    for (const id of ids) {
+        const div = document.createElement('div');
+        loadMedia(id, div);
+        mainMediaContainer.appendChild(div);
+    }
+} 
+
+document.querySelectorAll('.date-input').forEach(input => {
+    input.addEventListener('change', async () => {
+        const start = document.getElementById('startDate').value;
+        const end = document.getElementById('endDate').value;
+
+        if (!start || !end) return;
+
+        const startUnix = Math.floor(new Date(start).getTime() / 1000);
+        const endDate = new Date(end);
+        endDate.setHours(23, 59, 59, 999);
+        const endUnix = Math.floor(endDate.getTime() / 1000);
+
+        ids = await getIdRangeInt(startUnix, endUnix);
+        
+        loadMediaArray(ids);
+    });
+});
